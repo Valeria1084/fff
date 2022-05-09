b5869437a5f90e9b86aae8f20f29f76852045046
Valeria1084 <leraponomarenko1084@gmail.com>
2022-05-09 19:08:05 +0300

    lesson11

---
+++ lesson11/main.js
@@ -0,0 +1,32 @@
+//-створити форму з інпутами для name та age.
+// При відправці форми записати об'єкт в localstorage
+let name = document.forms.userForm.name;
+let age = document.forms.userForm.age;
+let btn = document.getElementById('btn')
+let key = 'key'
+
+let add = (userName,userAge) => {
+    let user = {
+        name: userName,
+        age: userAge
+    }
+    localStorage.setItem(key,JSON.stringify(user))
+}
+btn.onclick = () => {
+    add(name.value,age.value)
+}
+//-створити форму з інпутами для model,type та volume автівки.
+// при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+const {model,type,volume} = document.forms.carForm
+let btnCar = document.getElementById('btnCar');
+let item = 'kayCar'
+
+let add2 = (model,type,volume) => {
+    let arr = JSON.parse(localStorage.getItem(item)) || [];
+    arr.push({model,type,volome})
+    localStorage.setItem(item,JSON.stringify(arr))
+}
+    btnCar.onclick = () => {
+        add2(model.value,type.value,volume.value)
+    }
+
